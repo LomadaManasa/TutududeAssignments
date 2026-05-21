34e75e587beacadf6fec93bba04d98cdc87892ed
LomadaManasa <manasaganesh1712@gmail.com>
2026-05-21 11:54:59 +0530

    Updated Python

---
+++ Python_Assignment/CreateFile.py
@@ -0,0 +1,13 @@
+data = open ("sample.txt" , "x+")
+
+data.write ("Created a new file named sample.txt")
+
+data.write ("\nWorking for TuteDude Assignment Submussion")
+
+data.write ("\nIn Live mentorship will they help 3+ years experienced with the Resume and Job Oppurtunoties on course completion ?")
+
+print("File created successfully")
+
+data.close()
+
+
---
+++ Python_Assignment/Dictionary_operations.py
@@ -0,0 +1,7 @@
+Grades = {"Manasa" : "A" , "Ganesh" : "B" , "Nanda Kishor" : "A+"}
+
+Grades["Karthik"] = "B+"
+
+Grades["Manasa"] = "D"
+
+print (Grades)
